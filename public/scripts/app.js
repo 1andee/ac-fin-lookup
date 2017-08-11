abed92a99d212f9ce29bf04f001b50d4d4c978bf
Ian Duke <ian.c.duke@gmail.com>
2017-08-11 17:06:36 -0400

    Implemented fin search, response successfully renders on client-side

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,19 +1,70 @@
 $(() => {
 
-  $( "#search_bar .button" ).click(function() {
+  $("#search_bar .input-field").keypress((e) => {
+    if(e.which == 13) {
 
-    preventDefault();
+      e.preventDefault();
 
-    $('.search_results').empty();
+      $('.search_results').empty();
 
-    $.ajax({
-      url: '/',
-      type: 'POST',
-      success: ((result) => {
-        for (i in result ) {
-           $('.search_results').append(`<div>${i}</div>`);
-        };
+      let data = {};
+      data.title = "userSearch";
+      data.query = $('.input-field').val();
+      console.log(data);
+
+      $.ajax({
+        url: '/',
+        method: 'POST',
+        dataType: "json",
+        data: data
+      }).done((response) => {
+
+        let count = `<h5>Found ${response.length} matching aircraft</h5>`
+        $('.search_results').append(count);
+
+        response.forEach((element) => {
+          console.log(element);
+          let aircraft = createListElement(element);
+          $('.search_results').append(aircraft);
+
+        });
+
       });
-    });
+
+    };
   });
+
+  createListElement = (element) => {
+
+    if (element.operator === 'Air Canada') {
+      var logo = `<img src="favicon.ico" height="15px" title="Air Canada (Mainline)"/>`
+    } else if (element.operator === 'Rouge'){
+      var logo = `<img src="images/rouge.png" height="15px" title="Rouge"/>`
+    };
+
+    let row = `
+    <div class="row">
+      <div class="two columns">
+        <p>
+          Fin ${element.id}
+        </p>
+      </div>
+      <div class="two columns">
+        <p>
+          ${element.reg}
+        </p>
+      </div>
+      <div class="two columns">
+        <p>
+          ${element.specific_type}
+        </p>
+      </div>
+      <div class="two columns">
+        ${logo}
+      </div>
+    </div>
+    `
+    return row;
+  };
+
 });
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-"use strict";
+'use strict';
 
 require('dotenv').config();
 
 const PORT            = process.env.PORT || 8080;
-const ENV             = process.env.NODE_ENV || "development";
-const express         = require("express");
-const bodyParser      = require("body-parser");
+const ENV             = process.env.NODE_ENV || 'development';
+const express         = require('express');
+const bodyParser      = require('body-parser');
 const app             = express();
 
-const knexConfig  = require("./knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
+const knexConfig  = require('./knexfile');
+const knex        = require('knex')(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
 
@@ -19,31 +19,33 @@ app.use(morgan('dev'));
 // Log knex SQL queries to STDOUT
 app.use(knexLogger(knex));
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Home page
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.render('index');
 });
 
 // Search field
-app.post("/", (req, res) => {
-  console.log(`User searched for: ${req.body.search}`)
-  let result = search(req.body.search)
+app.post('/', (req, res) => {
+  console.log('req.body.query:', req.body.query);
+  let result = search(req.body.query)
   .then((result)=> {
+    console.log('====================');
+    console.log('====== RESULT ======');
     console.log(result)
     res.json(result);
-    console.log(`sent the result`)
+    console.log('====================');
   });
 });
 
 // Knex SQL search by fin # OR registration
 function search(id) {
-  return knex("fins").where("id", "like", `%${id}%`).orWhere("reg", "like", `%${id}%`);
+  return knex('fins').where('id', 'like', `%${id}%`).orWhere('reg', 'like', `%${id}%`).orderBy('id', 'asc');
 }
 
 app.listen(PORT, () => {
-  console.log("fin-lookup is listening on port " + PORT);
+  console.log('fin-lookup is listening on port ' + PORT);
 });
