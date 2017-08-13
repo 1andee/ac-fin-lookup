543e1c92ef14bbc7f954b93775e56baaebd9abe9
Ian Duke <ian.c.duke@gmail.com>
2017-08-13 00:55:55 -0400

    Lookup results rendering in table

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -21,49 +21,42 @@ $(() => {
         let count = `<h5>Found ${response.length} matching aircraft</h5>`
         $('.search_results').append(count);
 
-        response.forEach((element) => {
-          let aircraft = createListElement(element);
-          $('.search_results').append(aircraft);
+        let tableHead = `
+        <table id="results-table" class="u-full-width">
+          <thead>
+          <tr>
+            <th>Fin</th>
+            <th>Registration</th>
+            <th>Type</th>
+            <th>Operator</th>
+          </tr>
+          </thead>`
 
+        $('.search_results').append(tableHead);
+
+        response.forEach((element) => {
+
+          if (element.operator === 'Air Canada') {
+            var logo = `<img src="favicon.ico" height="15px" title="Air Canada (Mainline)"/>`
+          } else if (element.operator === 'Rouge'){
+            var logo = `<img src="images/rouge.png" height="15px" title="Rouge"/>`
+          };
+
+          $('#results-table').append(`
+            <tr>
+              <td>${element.id}</td>
+              <td>${element.reg}</td>
+              <td>${element.specific_type}</td>
+              <td>${logo}</td>
+            </tr>`)
         });
 
-      });
+        let tableEnd =
+        `</tbody>
+        </table>`
 
+        $('.results-table').append(tableEnd);
+      });
     };
   });
-
-  createListElement = (element) => {
-
-    if (element.operator === 'Air Canada') {
-      var logo = `<img src="favicon.ico" height="15px" title="Air Canada (Mainline)"/>`
-    } else if (element.operator === 'Rouge'){
-      var logo = `<img src="images/rouge.png" height="15px" title="Rouge"/>`
-    };
-
-    let row = `
-    <div class="row">
-      <div class="two columns">
-        <p>
-          Fin ${element.id}
-        </p>
-      </div>
-      <div class="two columns">
-        <p>
-          ${element.reg}
-        </p>
-      </div>
-      <div class="two columns">
-        <p>
-          ${element.specific_type}
-        </p>
-      </div>
-      <div class="two columns">
-        ${logo}
-      </div>
-    </div>
-    <hr>
-    `
-    return row;
-  };
-
 });
