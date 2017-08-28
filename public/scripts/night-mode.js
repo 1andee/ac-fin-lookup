0cffa3fe4334d47daa4564122ff2c94a55e20f78
Ian Duke <ian.c.duke@gmail.com>
2017-08-28 19:38:27 -0400

    JS months begin at 0

--- public/scripts/night-mode.js
+++ public/scripts/night-mode.js
@@ -6,23 +6,23 @@ $(() => {
 
   function timeCheck() {
 
-    if (currentMonth >= 1 && currentMonth <= 2) {
+    if (currentMonth <= 1) {
       if (currentHour < 8 || currentHour >= 17) {
         document.documentElement.classList.add('night');
       };
-    } else if (currentMonth >= 3 && currentMonth <= 4) {
+    } else if (currentMonth >= 2 && currentMonth <= 3) {
       if (currentHour < 7 || currentHour >= 20) {
         document.documentElement.classList.add('night');
       };
-    } else if (currentMonth >= 5 && currentMonth <= 8) {
+    } else if (currentMonth >= 4 && currentMonth <= 7) {
       if (currentHour < 6 || currentHour >= 21) {
         document.documentElement.classList.add('night');
       };
-    } else if (currentMonth >= 9 && currentMonth <= 11) {
+    } else if (currentMonth >= 8 && currentMonth <= 10) {
       if (currentHour < 7 || currentHour >= 18) {
         document.documentElement.classList.add('night');
       };
-    } else if (currentMonth >= 11) {
+    } else if (currentMonth >= 10) {
       if (currentHour < 8 || currentHour >= 17) {
         document.documentElement.classList.add('night');
       };
